f7f4a3921e50eba566ad346e633ddac3ec78c580
Emily <emilycastillogrim@gmail.com>
2026-05-10 21:27:51 -0700

    connected database

--- backend/riskheatmap.py
+++ backend/riskheatmap.py
@@ -1,33 +1,49 @@
 import pandas as pd
 import psycopg2
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-def generate_heatmap():
-    conn = psycopg2.connect(
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials= True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_connection():
+    return psycopg2.connect(
       host="localhost",
       database="healthcare",
       user="postgres",
       password="ott3r",
       port = 5432
 )
+    
+@app.get("/patients")
+def get_patients():
+    conn = get_connection()
+
     query = """
-    SELECT 
-      age,
-      (smoking + alcohol_intake - physical_activity) AS lifestyle_score,
-      heart_disease
+    SELECT
+      patient_id AS "Patient_ID",
+      age AS "Age",
+      gender AS "Gender",
+      bmi AS "BMI",
+      blood_pressure_systolic AS "Blood_Pressure_Systolic",
+      blood_pressure_diastolic AS "Blood_Pressure_Diastolic",
+      cholesterol AS "Cholesterol",
+      glucose_level AS "Glucose_Level",
+      smoking AS "Smoking",
+      alcohol_intake AS "Alcohol_Intake",
+      physical_activity AS "Physical_Activity",
+      family_history AS "Family_History",
+      heart_disease AS "Heart_Disease",
+      diabetes AS "Diabetes",
+      stroke AS "Stroke"
     FROM patient;"""
 
     df = pd.read_sql_query(query, conn)
     conn.close()
-    df['age_group'] = pd.cut(df['age'], bins= [0, 30, 50, 70, 120], labels=['0-30', '31-50', '51-70', '71+'], include_lowest=True)
-    df['lifestyle_group'] = pd.cut(df['lifestyle_score'], bins= [-10, -1, 1, 3, 10] , labels=['Healthy', 'Moderate', 'Risky', 'Very Risky'], include_lowest=True )
-
-    heatmap_data = df.pivot_table(index='lifestyle_group', columns='age_group', values='heart_disease', aggfunc='mean')
-    heatmap_data = heatmap_data.reindex(
-      index =['Healthy', 'Moderate', 'Risky', 'Very Risky'],
-      columns=['0-30', '31-50', '51-70', '71+']
-    )
-    return {
-      "x_labels": list(heatmap_data.columns.astype(str)),
-      "y_labels": list(heatmap_data.index.astype(str)),
-      "z_values": heatmap_data.fillna(0).values.tolist()
-}
+    return df.to_dict(orient="records")
